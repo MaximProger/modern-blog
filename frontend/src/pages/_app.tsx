00497a7e5a9e0eea2753b4099ca7f2f5f000745a
MaximProger <maksimkoshman090@gmail.com>
2023-05-12 16:07:29 +0300

    add global context

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,42 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { getStrapiMedia } from "./api/media";
+import { createContext } from "react";
+import { fetchAPI } from "./api";
+import App from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const GlobalContext = createContext({});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const { global } = pageProps;
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href={getStrapiMedia(global.attributes.favicon)}
+        />
+      </Head>
+      <GlobalContext.Provider value={global.attributes}>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
+    </>
+  );
 }
+
+MyApp.getInitialProps = async (ctx: any) => {
+  const appProps = await App.getInitialProps(ctx);
+
+  const globalRes = await fetchAPI("/global", {
+    populate: {
+      favicon: "*",
+      defaultSeo: {
+        populate: "*",
+      },
+    },
+  });
+
+  return { ...appProps, pageProps: { global: globalRes.data } };
+};
